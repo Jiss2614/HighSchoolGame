2581208396e833c36c18f75059864f591be1312b
Jiss2614 <naruksmi.perusona4@gmail.com>
2017-07-08 18:07:29 +0900

    added unity files

---
+++ Assets/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	public int takasa = 1;
+	public float mawaru = 2.5f;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (0, takasa, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (mawaru, 0 , 0);
+	}
+}
